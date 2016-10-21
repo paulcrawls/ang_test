65b1f0f2bf3ad8c5d43a91b14d3f1780f0a2b38b
paulcrawls <pkawol@gmail.com>
2016-10-21 18:47:47 +0300

    add: *

--- components/directives/lists.directive.js
+++ components/directives/lists.directive.js
@@ -10,10 +10,16 @@
     // Definition of directive
     var directiveDefinitionObject = {
       restrict: 'AE',
-      link: function($scope, element, attrs) {
+      link: function($scope, attrs) {
 
         $scope.orderProperty = "+info.name";
         $scope.sortingTag = "";
+
+        $scope.showInfo = function(element) {
+          $('.info .name span').html(element.info.name);
+          $('.info .flags div').empty();
+          $('.info .flags div').append($('#' + element.id).find('.flags').clone());
+        };
 
         $scope.sortProperty = function(column) {
           $('.sort').toggleClass('unchecked checked');
